5ed78eedb8a8a54cdd7fb6483e7da7f12dfbeb24
tarde <alvaro.miggon@educa.jcyl.es>
2021-04-29 17:59:36 +0200

    Pedir Datos

--- Tarea 4.2/src/EjecutarPersona.java
+++ Tarea 4.2/src/EjecutarPersona.java
@@ -3,7 +3,20 @@ public class EjecutarPersona {
  
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    
+    System.out.println("Introduce el nombre");
+    String nombre = sc.next();
+
+    System.out.println("Introduce la edad");
+    int edad = sc.nextInt();
+
+    System.out.println("Introduce el sexo");
+    char sexo = sc.next().charAt(0);
+
+    System.out.println("Introduce el peso");
+    double peso = sc.nextDouble();
+
+    System.out.println("Introduce la altura");
+    double altura = sc.nextDouble();
     
     
     }
