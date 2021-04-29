6f821700e56e88cba8732ce782b34f4e848a7020
vero508 <veronica.buejus@educa.jcyl.es>
2021-04-29 11:32:08 +0200

    primera subida

---
+++ Tarea 4.2/src/Persona.java
@@ -0,0 +1,16 @@
+
+public class Persona {
+
+	private String nombre;
+	private int edad;
+	private String DNI;
+	private char sexo;
+	private double peso;
+	private double altura;
+	
+	private final static char SEXO_DEF='H';
+
+	public final static int INFRAPESO= -1;
+	public final static int PESO_IDEAL= 0;
+	public final static int SOBREPESO= 1;
+}
