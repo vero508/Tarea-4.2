a5b4cdb9fad9e1c0a45ba022699e0e9402544a14
veronica.buejus <veronica.buejus@educa.jcyl.es>
2021-04-29 17:38:20 +0200

    segunda subida

--- Tarea 4.2/src/Persona.java
+++ Tarea 4.2/src/Persona.java
@@ -3,14 +3,22 @@ public class Persona {
 
 	private String nombre;
 	private int edad;
-	private String DNI;
-	private char sexo;
 	private double peso;
 	private double altura;
 	
-	private final static char SEXO_DEF='H';
 
 	public final static int INFRAPESO= -1;
 	public final static int PESO_IDEAL= 0;
 	public final static int SOBREPESO= 1;
+	
+	
+	public Persona(String nombre, int edad, double peso, double altura) {
+		
+		this.nombre = nombre;
+		this.edad = edad;
+		this.peso = peso;
+		this.altura = altura;
+	}
+	
+	
 }
